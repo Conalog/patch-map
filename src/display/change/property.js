7ef652f611af7df6ee73529208833c0677196c29
MinCrohn <dev.mincrohn@gmail.com>
2025-03-26 14:57:51 +0900

    fix changeProperty error

--- src/display/change/property.js
+++ src/display/change/property.js
@@ -3,6 +3,6 @@ import { updateConfig } from './utils';
 export const changeProperty = (object, key, value) => {
   if (key === 'metadata' || key in object) {
     object[key] = value;
-    updateConfig(object, { key: value });
+    updateConfig(object, { [key]: value });
   }
 };
