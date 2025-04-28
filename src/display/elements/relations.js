45deef38575bca4fb3e8bf8ae09283d89b765ce7
MinCrohn <dev.mincrohn@gmail.com>
2025-04-28 12:33:59 +0900

    add elements validate logic

--- src/display/elements/relations.js
+++ src/display/elements/relations.js
@@ -1,5 +1,8 @@
 import { Graphics } from 'pixi.js';
+import { isValidationError } from 'zod-validation-error';
+import { validate } from '../../utils/vaildator';
 import { elementPipeline } from '../change/pipeline/element';
+import { relationGroupObject } from '../data-schema/data-schema';
 import { updateObject } from '../update/update-object';
 import { createContainer } from '../utils';
 
@@ -12,6 +15,8 @@ export const createRelations = (config) => {
 
 const pipelineKeys = ['show', 'strokeStyle', 'links'];
 export const updateRelations = (element, config, options) => {
+  const validateConfig = validate(config, relationGroupObject.partial());
+  if (isValidationError(validateConfig)) throw validateConfig;
   updateObject(element, config, elementPipeline, pipelineKeys, options);
 };
 
