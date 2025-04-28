2bf427d97bbb66ae3c8823506f9b5ac7ad5842f3
MinCrohn <dev.mincrohn@gmail.com>
2025-04-28 16:43:47 +0900

    fix property func

--- src/display/change/property.js
+++ src/display/change/property.js
@@ -1,5 +1,5 @@
 import { deepMerge } from '../../utils/deepmerge/deepmerge';
 
 export const changeProperty = (object, key, value) => {
-  object[key] = deepMerge(object[key], value);
+  deepMerge(object, { [key]: value });
 };
