25cee04c81d12a70960b094ece1a41a7e22b4a31
MinCrohn <dev.mincrohn@gmail.com>
2025-04-30 15:52:51 +0900

    kill prev animation

--- src/display/change/animation.js
+++ src/display/change/animation.js
@@ -1,10 +1,9 @@
-import gsap from 'gsap';
-import { isConfigMatch, updateConfig } from './utils';
+import { isConfigMatch, tweensOf, updateConfig } from './utils';
 
 export const changeAnimation = (object, { animation }) => {
   if (isConfigMatch(object, 'animation', animation)) {
     return;
   }
-  gsap.getTweensOf(object).forEach((tween) => tween.progress(1).kill());
+  tweensOf(object).forEach((tween) => tween.progress(1).kill());
   updateConfig(object, { animation });
 };
--- src/display/change/utils.js
+++ src/display/change/utils.js
@@ -1,3 +1,4 @@
+import gsap from 'gsap';
 import { deepMerge } from '../../utils/deepmerge/deepmerge';
 import { isSame } from '../../utils/diff/isSame';
 
@@ -8,3 +9,5 @@ export const isConfigMatch = (object, key, value) => {
 export const updateConfig = (object, config) => {
   object.config = deepMerge(object.config, config);
 };
+
+export const tweensOf = (object) => gsap.getTweensOf(object);
