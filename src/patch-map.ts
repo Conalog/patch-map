81fd91b8c7ddff66d607ae5a233d02546fb5af99
MinHo Lim <mhlim@conalog.com>
2025-06-11 11:34:25 +0900

    fix export undoredomanger

--- src/patch-map.ts
+++ src/patch-map.ts
@@ -1,4 +1,4 @@
 export { Patchmap } from './patchmap';
-export { undoRedoManager } from './command';
+export { UndoRedoManager } from './command/undo-redo-manager';
 export { Command } from './command/commands/base';
 export * as change from './display/change';
