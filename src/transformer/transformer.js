ce7684de3b395cab97e59d301589e38531052f25
MinHo Lim <mhlim@conalog.com>
2025-06-19 19:13:56 +0900

    refactor orientedBounds func

--- src/transformer/transformer.js
+++ src/transformer/transformer.js
@@ -1,12 +1,16 @@
 import { OrientedBounds } from '@pixi-essentials/bounds';
-import { Matrix, Point, RenderContainer, Transform } from 'pixi.js';
+import { Matrix, RenderContainer, Transform } from 'pixi.js';
 import { decomposeTransform } from './decomposeTransform';
+import {
+  getBoundsFromPoints,
+  getCentroid,
+  getObjectWorldCorners,
+} from './helper';
 import { Wireframe } from './wireframe';
 
 const tempBounds = new OrientedBounds();
 const tempTransform = new Transform();
 const tempMatrix = new Matrix();
-const tempCorners = [new Point(), new Point(), new Point(), new Point()];
 
 export class Transformer extends RenderContainer {
   constructor(options = {}) {
@@ -76,68 +80,24 @@ export class Transformer extends RenderContainer {
 
     decomposeTransform(tempTransform, displayObject.worldTransform);
     const worldRotation = tempTransform.rotation;
+    const worldCorners = getObjectWorldCorners(displayObject);
+    const centroid = getCentroid(worldCorners);
 
-    const localBounds = displayObject.getLocalBounds();
-    const corners = tempCorners;
-    corners[0].set(localBounds.x, localBounds.y);
-    corners[1].set(localBounds.x + localBounds.width, localBounds.y);
-    corners[2].set(
-      localBounds.x + localBounds.width,
-      localBounds.y + localBounds.height,
-    );
-    corners[3].set(localBounds.x, localBounds.y + localBounds.height);
-
-    const matrix = displayObject.worldTransform;
-    matrix.apply(corners[0], corners[0]);
-    matrix.apply(corners[1], corners[1]);
-    matrix.apply(corners[2], corners[2]);
-    matrix.apply(corners[3], corners[3]);
-
-    const cx = (corners[0].x + corners[1].x + corners[2].x + corners[3].x) / 4;
-    const cy = (corners[0].y + corners[1].y + corners[2].y + corners[3].y) / 4;
     const unrotateMatrix = tempMatrix;
     unrotateMatrix
       .identity()
-      .translate(-cx, -cy)
+      .translate(-centroid.x, -centroid.y)
       .rotate(-worldRotation)
-      .translate(cx, cy);
+      .translate(centroid.x, centroid.y);
+    unrotateMatrix.apply(worldCorners[0], worldCorners[0]);
+    unrotateMatrix.apply(worldCorners[1], worldCorners[1]);
+    unrotateMatrix.apply(worldCorners[2], worldCorners[2]);
+    unrotateMatrix.apply(worldCorners[3], worldCorners[3]);
 
-    unrotateMatrix.apply(corners[0], corners[0]);
-    unrotateMatrix.apply(corners[1], corners[1]);
-    unrotateMatrix.apply(corners[2], corners[2]);
-    unrotateMatrix.apply(corners[3], corners[3]);
-
-    const minX = Math.min(
-      corners[0].x,
-      corners[1].x,
-      corners[2].x,
-      corners[3].x,
-    );
-    const minY = Math.min(
-      corners[0].y,
-      corners[1].y,
-      corners[2].y,
-      corners[3].y,
-    );
-    const maxX = Math.max(
-      corners[0].x,
-      corners[1].x,
-      corners[2].x,
-      corners[3].x,
-    );
-    const maxY = Math.max(
-      corners[0].y,
-      corners[1].y,
-      corners[2].y,
-      corners[3].y,
-    );
-
+    const innerBounds = getBoundsFromPoints(worldCorners);
     const resultBounds = bounds || new OrientedBounds();
     resultBounds.rotation = worldRotation;
-    resultBounds.innerBounds.x = minX;
-    resultBounds.innerBounds.y = minY;
-    resultBounds.innerBounds.width = maxX - minX;
-    resultBounds.innerBounds.height = maxY - minY;
+    resultBounds.innerBounds.copyFrom(innerBounds);
     resultBounds.update();
     return resultBounds;
   }
@@ -147,51 +107,13 @@ export class Transformer extends RenderContainer {
       return;
     }
 
-    const allCorners = [];
-    for (let i = 0; i < group.length; i++) {
-      const element = group[i];
-      const localBounds = element.getLocalBounds();
-      const elementCorners = [
-        new Point(),
-        new Point(),
-        new Point(),
-        new Point(),
-      ];
-
-      elementCorners[0].set(localBounds.x, localBounds.y);
-      elementCorners[1].set(localBounds.x + localBounds.width, localBounds.y);
-      elementCorners[2].set(
-        localBounds.x + localBounds.width,
-        localBounds.y + localBounds.height,
-      );
-      elementCorners[3].set(localBounds.x, localBounds.y + localBounds.height);
-
-      const matrix = element.worldTransform;
-      matrix.apply(elementCorners[0], elementCorners[0]);
-      matrix.apply(elementCorners[1], elementCorners[1]);
-      matrix.apply(elementCorners[2], elementCorners[2]);
-      matrix.apply(elementCorners[3], elementCorners[3]);
-      allCorners.push(...elementCorners);
-    }
-
-    let minX = Number.POSITIVE_INFINITY;
-    let minY = Number.POSITIVE_INFINITY;
-    let maxX = Number.NEGATIVE_INFINITY;
-    let maxY = Number.NEGATIVE_INFINITY;
-    for (let i = 0; i < allCorners.length; i++) {
-      const point = allCorners[i];
-      minX = point.x < minX ? point.x : minX;
-      minY = point.y < minY ? point.y : minY;
-      maxX = point.x > maxX ? point.x : maxX;
-      maxY = point.y > maxY ? point.y : maxY;
-    }
-
+    const allWorldCorners = group.flatMap((element) => {
+      return getObjectWorldCorners(element);
+    });
+    const groupInnerBounds = getBoundsFromPoints(allWorldCorners);
     const resultBounds = bounds || new OrientedBounds();
     resultBounds.rotation = 0;
-    resultBounds.innerBounds.x = minX;
-    resultBounds.innerBounds.y = minY;
-    resultBounds.innerBounds.width = maxX - minX;
-    resultBounds.innerBounds.height = maxY - minY;
+    resultBounds.innerBounds.copyFrom(groupInnerBounds);
     resultBounds.update();
     return resultBounds;
   }
