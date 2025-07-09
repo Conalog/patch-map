f3e7f02ae6e33997e1b210378b96fde173c54ff5
MinHo Lim <mhlim@conalog.com>
2025-07-09 11:53:10 +0900

    fix source key

--- src/display/components/Icon.js
+++ src/display/components/Icon.js
@@ -9,7 +9,7 @@ import { Tintable } from '../mixins/Tintable';
 import { mixins } from '../mixins/utils';
 
 const EXTRA_KEYS = {
-  PLACEMENT: ['size'],
+  PLACEMENT: ['source', 'size'],
 };
 
 const ComposedIcon = mixins(
