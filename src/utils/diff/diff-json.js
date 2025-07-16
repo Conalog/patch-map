88f5cd35a9f8e642049c3fdd271f79dc7aad96dd
MinHo Lim <mhlim@conalog.com>
2025-07-16 14:32:14 +0900

    fix diffjson

--- src/utils/diff/diff-json.js
+++ src/utils/diff/diff-json.js
@@ -1,8 +1,26 @@
 import { isPlainObject } from 'is-plain-object';
 
 export const diffJson = (obj1, obj2) => {
+  if (obj1 === obj2) return {};
   if (obj1 != null && obj2 == null) return obj1;
   if (obj1 == null && obj2 != null) return obj2;
+
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    if (obj1.length !== obj2.length) {
+      return obj2;
+    }
+
+    const areArraysEqual = obj1.every((item, index) => {
+      const itemDiff = diffJson(item, obj2[index]);
+      return (
+        typeof itemDiff === 'object' &&
+        itemDiff !== null &&
+        Object.keys(itemDiff).length === 0
+      );
+    });
+
+    return areArraysEqual ? {} : obj2;
+  }
 
   if (!isPlainObject(obj1) || !isPlainObject(obj2)) {
     return obj1 === obj2 ? {} : obj2;
