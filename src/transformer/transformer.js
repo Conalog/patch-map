172a8a0f3a6d3530cc23e1b24ec12fc4b7e815de
MinHo Lim <mhlim@conalog.com>
2025-07-23 18:31:43 +0900

    add transformer

---
+++ src/transformer/transformer.js
@@ -0,0 +1,100 @@
+import { Container } from 'pixi.js';
+import { calcGroupOrientedBounds, calcOrientedBounds } from '../utils/bounds';
+import { Wireframe } from './wireframe';
+
+const DEFAULT_WIREFRAME_STYLE = {
+  thickness: 1.5,
+  color: '#1099FF',
+};
+
+export class Transformer extends Container {
+  constructor(options = {}) {
+    super({});
+    this.zIndex = 100;
+    this.lazyTrigger = true;
+    this.wireframe = this.addChild(new Wireframe(this));
+    this.onRender = this._refresh.bind(this);
+
+    this._elements = options.elements || [];
+    this.lazyMode = options.lazyMode || false;
+    this._wireframeStyle = Object.assign(
+      DEFAULT_WIREFRAME_STYLE,
+      options.wireframeStyle || {},
+    );
+    this.boundsDisplayMode = options.boundsDisplayMode || 'all'; // 'all | 'groupOnly' | 'elementOnly' | 'none'
+  }
+
+  get elements() {
+    return this._elements;
+  }
+
+  set elements(value) {
+    this._elements = Array.isArray(value) ? value : [value];
+    if (this.lazyMode) {
+      this.update();
+    }
+  }
+
+  get wireframeStyle() {
+    return this._wireframeStyle;
+  }
+
+  set wireframeStyle(value) {
+    this._wireframeStyle = Object.assign(this._wireframeStyle, value);
+  }
+
+  destroy(options) {
+    this.onRender = null;
+    super.destroy(options);
+  }
+
+  _refresh() {
+    if (
+      this.renderable &&
+      this.visible &&
+      (!this.lazyMode || this.lazyTrigger)
+    ) {
+      this.draw();
+    }
+  }
+
+  draw() {
+    const elements = this.elements;
+    if (!elements) {
+      return;
+    }
+
+    const { color, thickness } = this._wireframeStyle;
+    this.wireframe.clear();
+    if (this.boundsDisplayMode !== 'none') {
+      this.wireframe.setStrokeStyle({
+        width: thickness / this.parent.scale.x,
+        color,
+      });
+    }
+
+    if (
+      this.boundsDisplayMode === 'all' ||
+      this.boundsDisplayMode === 'elementOnly'
+    ) {
+      elements.forEach((element) => {
+        this.wireframe.drawBounds(calcOrientedBounds(element));
+      });
+    }
+
+    if (
+      this.boundsDisplayMode === 'all' ||
+      this.boundsDisplayMode === 'groupOnly'
+    ) {
+      const groupBounds =
+        elements.length > 1 ? calcGroupOrientedBounds(elements) : null;
+      this.wireframe.drawBounds(groupBounds);
+    }
+
+    this.lazyTrigger = false;
+  }
+
+  update() {
+    this.lazyTrigger = true;
+  }
+}
