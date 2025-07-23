e104eff976a8fab3b459e4b5a977182d7f00af59
MinHo Lim <mhlim@conalog.com>
2025-07-23 18:30:54 +0900

    add wireframe

---
+++ src/transformer/wireframe.js
@@ -0,0 +1,18 @@
+import { Graphics } from 'pixi.js';
+
+export class Wireframe extends Graphics {
+  constructor(transformer) {
+    super();
+    this.transformer = transformer;
+  }
+
+  drawBounds(bounds) {
+    if (bounds) {
+      const hull = bounds.hull.map((worldPoint) => {
+        return this.toLocal(worldPoint);
+      });
+
+      this.poly(hull).stroke();
+    }
+  }
+}
