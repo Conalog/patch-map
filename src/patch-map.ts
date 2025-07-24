a15d14cae4769acb531af714b47e5979a897ce1f
MinHo Lim <mhlim@conalog.com>
2025-07-24 12:22:23 +0900

    export Transformer

--- src/patch-map.ts
+++ src/patch-map.ts
@@ -1,3 +1,4 @@
 export { Patchmap } from './patchmap';
 export { UndoRedoManager } from './command/undo-redo-manager';
 export { Command } from './command/commands/base';
+export { Transformer } from './transformer/transformer';
