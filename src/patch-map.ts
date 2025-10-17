0a43ea06b150eb4832074e2fad7c5dde8fa6bbeb
MinHo Lim <mhlim@conalog.com>
2025-10-17 10:20:26 +0900

    chore: export selector (#138)

--- src/patch-map.ts
+++ src/patch-map.ts
@@ -3,4 +3,5 @@ export { UndoRedoManager } from './command/UndoRedoManager';
 export { Command } from './command/commands/base';
 export { default as Transformer } from './transformer/Transformer';
 export { default as State, PROPAGATE_EVENT } from './events/states/State';
+export { selector } from './utils/selector/selector';
 export * from './utils';
