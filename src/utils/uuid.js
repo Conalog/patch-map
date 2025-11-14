790d89df136c82bf7969a4ee4bcb5f804580a017
MinHo Lim <mhlim@conalog.com>
2025-11-14 17:22:11 +0900

    fix

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -3,8 +3,8 @@ import { customAlphabet } from 'nanoid';
 // uuidv4 provides completely random values. (CSPRNG)
 // nanoid includes uppercase alphabets, making it more secure (completely random like uuidv4).
 // nanoid allows direct length adjustment in the function.
-export const uid = () =>
-  customAlphabet(
-    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-    15,
-  )();
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  15,
+);
+export const uid = () => nanoid();
