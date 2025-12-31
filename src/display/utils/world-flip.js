7e0ce462cb1cb5920ab4fe10fdffa84e78e7cb83
perhapsspy <perhapsspy@gmail.com>
2025-12-31 16:05:43 +0900

    fix: stabilize flipped text/icon positioning

--- src/display/utils/world-flip.js
+++ src/display/utils/world-flip.js
@@ -5,16 +5,18 @@ export const applyWorldFlip = (displayObject, view) => {
   const nextState = { x: !!view.flipX, y: !!view.flipY };
   const width = displayObject.width ?? 0;
   const height = displayObject.height ?? 0;
+  const prevWidth = prevState.width ?? width;
+  const prevHeight = prevState.height ?? height;
   const absScaleX = Math.abs(displayObject.scale?.x ?? 1);
   const absScaleY = Math.abs(displayObject.scale?.y ?? 1);
 
   let baseX = displayObject.x;
   let baseY = displayObject.y;
   if (prevState.x) {
-    baseX -= width;
+    baseX -= prevWidth;
   }
   if (prevState.y) {
-    baseY -= height;
+    baseY -= prevHeight;
   }
 
   displayObject.scale.set(
@@ -25,5 +27,10 @@ export const applyWorldFlip = (displayObject, view) => {
     nextState.x ? baseX + width : baseX,
     nextState.y ? baseY + height : baseY,
   );
-  displayObject._flipState = nextState;
+  displayObject._flipState = {
+    x: nextState.x,
+    y: nextState.y,
+    width,
+    height,
+  };
 };
